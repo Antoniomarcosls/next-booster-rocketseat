38b67c9556bd8bb9e609ec35836f58f66434eb16
Antonio Marcos <antoniomarcosleite5@gmail.com>
2020-06-19 16:38:12 -0300

    COMMIT 16-06-20 16:38

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,30 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express();
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
+const users = [
+    "Antonio",
+    "Daniel",
+    "Diego",
+    "Roberto"
+]
 
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    response.json([
-        "Antonio",
-        "Marcos",
-        "Diego",
-        "Roberto"
-    ]);
+    return response.json(users);
 });
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Gordo',
+        email: 'gordo@gmail.com'
+    }
+
+    return response.json(user)
+})
 
 app.listen(3333);
